3e27d05bcf7586b142b638fa99979e4b308817f3
Equxp <manatimanati8800@gmail.com>
2022-05-22 15:08:51 +0300

    Update clickeer.py

--- clickeer.py
+++ clickeer.py
@@ -3,12 +3,22 @@ from selenium.webdriver.common.by import By
 import time
 
 def get_scores(scores):
-    "Get available score"
+    "Get available cookie score number"
     try:
         num = int(scores.text.split()[0])
         return num
     except ValueError:
         return 0
+
+def click_prod0(browser, scores):
+    """clickthe first product"""
+    try:
+        price = int(browser.find_element(By.ID, 'productPrice0').text)
+    except ValueError:
+        price = 0
+        if price and price <= get_scores(scores):
+            prod0 = browser.find_element(By.ID, 'product0')
+            prod0.click()
 
 def main():
     browser = webdriver.Chrome('F:\Programing Projects\Python Projects\cookieclicker\chromedriver.exe')
@@ -21,6 +31,7 @@ def main():
         time.sleep(0.1)
         num = get_scores(scores)
         upgrades = browser.find_elements(By.CLASS_NAME, "upgrade")
+        click_prod0(browser, scores)
         if num > 100 and len(upgrades)>1:
             upgrades[0].click()
             print("upgraded!!!")
